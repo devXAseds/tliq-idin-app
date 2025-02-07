82f57505357bba67c49a4708fe5b7774ffe1080f
Bouhlal <yuunpim@gmail.com>
2025-02-07 09:15:05 +0100

    sec com

---
+++ src/domain/_business/domainEx/domainEx.service.ts
@@ -0,0 +1,7 @@
+import { DomainInterface } from "@mahdi/mahdi-lib/interfaces/domain.interface";
+
+export class DomainExService {
+    async createDomainEx(domain: DomainInterface): Promise<Result<DomainInterface | Result<Error>> {
+
+    }
+}
